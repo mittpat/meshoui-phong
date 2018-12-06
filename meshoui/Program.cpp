8b0d2545fa403fcb3c07210131be55511ba4726c
Patrick Pelletier <patrick.pelletier@cm-labs.com>
2018-12-05 22:34:21 -0500

    SpirV is binary...

--- meshoui/Program.cpp
+++ meshoui/Program.cpp
@@ -15,12 +15,12 @@ void Program::load(const std::string & filename)
         {
             if (pair[0] == "vertexShaderFilename")
             {
-                std::ifstream fileStream(sibling(pair[1], filename));
+                std::ifstream fileStream(sibling(pair[1], filename), std::ifstream::binary);
                 vertexShaderSource = std::vector<char>((std::istreambuf_iterator<char>(fileStream)), std::istreambuf_iterator<char>());
             }
             if (pair[0] == "fragmentShaderFilename")
             {
-                std::ifstream fileStream(sibling(pair[1], filename));
+                std::ifstream fileStream(sibling(pair[1], filename), std::ifstream::binary);
                 fragmentShaderSource = std::vector<char>((std::istreambuf_iterator<char>(fileStream)), std::istreambuf_iterator<char>());
             }
         }
